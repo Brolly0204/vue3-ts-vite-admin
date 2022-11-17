4bce07399c43bfa661c5ec9d8d7be0f8fb32b961
brolly <102495553@qq.com>
2022-11-17 15:33:04 +0800

    build: 项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/eslint-recommended',
+    '@vue/prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true, // 使用单引号
+        semi: false, // 末尾添加分号
+        tabWidth: 2,
+        trailingComma: 'none',
+        useTabs: false,
+        endOfLine: 'auto'
+      }
+    ]
+  }
+}
