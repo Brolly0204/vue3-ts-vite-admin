ec03cfde0c0498d4c2935ca48a3435be6bfd09af
brolly <102495553@qq.com>
2022-11-21 17:14:24 +0800

    feat: layout布局

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router/index'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+// 初始化css，重置css默认样式
+import 'normalize.css/normalize.css'
+// 全局css
+import '@/styles/index.scss'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
